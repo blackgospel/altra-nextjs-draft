ad26fb2128ba7fe49237a30bfca352bc9d1a276f
Seun Adesina <sadesina.96@gmail.com>
2021-12-14 12:34:00 +0000

    added some styling changes to section

--- src/components/home/info-card/index.info-card.tsx
+++ src/components/home/info-card/index.info-card.tsx
@@ -1,4 +1,5 @@
 import Button from 'components/atomic/button/atomic.button'
+import Spacing from 'components/atomic/spacing/atomic.spacing'
 import Section from 'components/global/section/index.section'
 import styles from './index.module.sass'
 
@@ -6,14 +7,22 @@ const InfoCard: React.FC = () => {
   return (
     <Section>
       <div className={styles.container}>
-        <h1 className={styles.headline}>For the whole team </h1>
-        <p className={styles.subtitle}>
-          Automation works when it`s built by those who know the workflow best -
-          not distant developers. Tines is no-code, giving your frontline team
-          members automation superpowers.
-        </p>
-        <div className={styles[`button-wrapper`]}>
-          <Button />
+        <div className={styles.wrapper}>
+          <h1 className={styles.headline}>For the whole team </h1>
+
+          <Spacing />
+
+          <p className={styles.subtitle}>
+            Automation works when it`s built by those who know the workflow best
+            - not distant developers. Tines is no-code, giving your frontline
+            team members automation superpowers.
+          </p>
+
+          <Spacing />
+
+          <div className={styles[`button-wrapper`]}>
+            <Button>Meet the product</Button>
+          </div>
         </div>
       </div>
     </Section>
