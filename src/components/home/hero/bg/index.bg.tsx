60a07a35c7b090733c71a7629000e728e553bfbc
Seun Adesina <sadesina.96@gmail.com>
2021-12-18 14:56:45 +0000

    readded animations

---
+++ src/components/home/hero/bg/index.bg.tsx
@@ -0,0 +1,18 @@
+import styles from './index.module.sass'
+
+const HeroBg: React.FC = () => {
+  return (
+    <div className={styles.background}>
+      <ul className={styles.wrapper}>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+    </div>
+  )
+}
+
+export default HeroBg
