c7d93dce859dd45ce45e4fbb8fdbadedce7d1b29
Seun Adesina <sadesina.96@gmail.com>
2021-12-19 08:18:02 +0000

    added footer to customer page

--- src/pages/customers.tsx
+++ src/pages/customers.tsx
@@ -2,6 +2,7 @@ import CustomersHero from 'components/customers/hero/customers.hero'
 import Stories from 'components/customers/stories/home.stories'
 import Page from 'components/global/page/index.page'
 import Companies from 'components/shared/companies/index.companies'
+import Footer from 'components/shared/footer/index.footer'
 
 const Customers = () => {
   return (
@@ -9,6 +10,7 @@ const Customers = () => {
       <CustomersHero />
       <Companies />
       <Stories />
+      <Footer />
     </Page>
   )
 }
