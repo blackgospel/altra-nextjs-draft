a9ab1747c8e546aa18eb821954f20110e9bedf59
Seun Adesina <sadesina.96@gmail.com>
2021-12-21 07:20:52 +0000

    added basic layout for contact page

---
+++ src/components/contact/hero/contacts.hero.tsx
@@ -0,0 +1,13 @@
+import Hero from 'components/shared/hero/index.hero'
+
+const ContactsHero: React.FC = () => {
+  return (
+    <Hero
+      title="Have a family member in a care home?"
+      description="Do you like the idea and want to share it with the care home manager? We`ve made it easy to send them an email about our service."
+      color="secondary"
+    />
+  )
+}
+
+export default ContactsHero
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,9 +1,11 @@
+import ContactsHero from 'components/contact/hero/contacts.hero'
 import Page from 'components/global/page/index.page'
 import Footer from 'components/shared/footer/index.footer'
 
 const Contact = () => {
   return (
     <Page title="Contact">
+      <ContactsHero />
       <Footer />
     </Page>
   )
