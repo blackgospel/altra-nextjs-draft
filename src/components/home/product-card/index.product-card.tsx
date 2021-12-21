6b73826a8310147391b60f3a899887ad89b5610e
Seun Adesina <sadesina.96@gmail.com>
2021-12-21 07:06:08 +0000

    added links for buttons on product cards

--- src/components/home/product-card/index.product-card.tsx
+++ src/components/home/product-card/index.product-card.tsx
@@ -11,53 +11,55 @@ import styles from './index.module.sass'
 const ProductCard: React.FC = () => {
   return (
     <>
-      {HomeProductData.map(({ title, description, list, color }, index) => {
-        return (
-          <Section key={`product-card-${title}`} bubbleLeft1={index === 1}>
-            <div className={styles.container}>
-              <div className={styles.wrapper}>
-                <Card
-                  {...(color && {
-                    primary: color === 'primary',
-                    secondary: color === 'secondary',
-                    accent: color === 'accent',
-                  })}
-                >
-                  <h1 className={styles.headline}>{title}</h1>
+      {HomeProductData.map(
+        ({ title, description, list, color, link }, index) => {
+          return (
+            <Section key={`product-card-${title}`} bubbleLeft1={index === 1}>
+              <div className={styles.container}>
+                <div className={styles.wrapper}>
+                  <Card
+                    {...(color && {
+                      primary: color === 'primary',
+                      secondary: color === 'secondary',
+                      accent: color === 'accent',
+                    })}
+                  >
+                    <h1 className={styles.headline}>{title}</h1>
 
-                  <Spacing />
+                    <Spacing />
 
-                  <p className={styles.subtitle}>{description}</p>
+                    <p className={styles.subtitle}>{description}</p>
 
-                  <Spacing />
+                    <Spacing />
 
-                  <List
-                    title="product"
-                    list={list}
-                    secondary={color === 'secondary'}
-                  />
+                    <List
+                      title="product"
+                      list={list}
+                      secondary={color === 'secondary'}
+                    />
 
-                  <Spacing />
+                    <Spacing />
 
-                  <div className={styles.button_wrapper}>
-                    <Button lg secondary={color === 'primary'}>
-                      Meet the product
-                    </Button>
-                  </div>
-
-                  <Spacing />
-
-                  <div className={styles.image_wrapper}>
-                    <div className={styles.image}>
-                      <Image src={ProductImage} alt={title} />
+                    <div className={styles.button_wrapper}>
+                      <Button href={link} lg secondary={color === 'primary'}>
+                        Meet the product
+                      </Button>
                     </div>
-                  </div>
-                </Card>
+
+                    <Spacing />
+
+                    <div className={styles.image_wrapper}>
+                      <div className={styles.image}>
+                        <Image src={ProductImage} alt={title} />
+                      </div>
+                    </div>
+                  </Card>
+                </div>
               </div>
-            </div>
-          </Section>
-        )
-      })}
+            </Section>
+          )
+        }
+      )}
     </>
   )
 }
