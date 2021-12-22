11d28b97dd428d428c88eeee81e39fd28cb2674e
Seun Adesina <sadesina.96@gmail.com>
2021-12-22 22:57:01 +0000

    added contact content

---
+++ src/components/contact/form/contacts.form.tsx
@@ -0,0 +1,26 @@
+import Button from 'components/global/button/index.button'
+import Section from 'components/global/section/index.section'
+import styles from './index.module.sass'
+
+const ContactsForm: React.FC = () => {
+  return (
+    <Section>
+      <div className={styles.container}>
+        <input className={styles.input} placeholder="Your name" />
+        <input className={styles.input} placeholder="Your e-mail address" />
+        <input
+          className={styles.input}
+          placeholder="Family member`s name in care home"
+        />
+        <input
+          className={styles.input}
+          placeholder="Care home`s email address"
+        />
+        <input className={styles.input} placeholder="Personal message" />
+        <Button lg>Submit Details</Button>
+      </div>
+    </Section>
+  )
+}
+
+export default ContactsForm
--- src/pages/contact.tsx
+++ src/pages/contact.tsx
@@ -1,3 +1,4 @@
+import ContactsForm from 'components/contact/form/contacts.form'
 import ContactsHero from 'components/contact/hero/contacts.hero'
 import Page from 'components/global/page/index.page'
 import Footer from 'components/shared/footer/index.footer'
@@ -6,6 +7,7 @@ const Contact = () => {
   return (
     <Page title="Contact">
       <ContactsHero />
+      <ContactsForm />
       <Footer />
     </Page>
   )
