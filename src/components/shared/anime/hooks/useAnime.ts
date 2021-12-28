2c0ea18ce23958aa098fd9e9294af46c8e769e45
Seun Adesina <sadesina.96@gmail.com>
2021-12-28 21:07:11 +0000

    fixed animations on mobile

--- src/components/shared/anime/hooks/useAnime.ts
+++ src/components/shared/anime/hooks/useAnime.ts
@@ -1,13 +1,18 @@
 import { useAnimation } from 'framer-motion'
 import { useEffect } from 'react'
 import { useInView } from 'react-intersection-observer'
-import { DEFAULT_ROOT_MARGIN } from 'src/variants/tools'
+import useWindowSize from 'src/hooks/useWindowSize'
+import {
+  DEFAULT_MOBILE_ROOT_MARGIN,
+  DEFAULT_ROOT_MARGIN,
+} from 'src/variants/tools'
 
 const useAnime = () => {
+  const { width } = useWindowSize()
   const animation = useAnimation()
   const { ref, inView } = useInView({
     triggerOnce: true,
-    rootMargin: DEFAULT_ROOT_MARGIN,
+    rootMargin: width > 500 ? DEFAULT_ROOT_MARGIN : DEFAULT_MOBILE_ROOT_MARGIN,
   })
 
   useEffect(() => {
--- src/variants/tools.ts
+++ src/variants/tools.ts
@@ -1,5 +1,7 @@
 export const DEFAULT_ROOT_MARGIN = '-300px'
 
+export const DEFAULT_MOBILE_ROOT_MARGIN = '-50px'
+
 export const DEFAULT_EASING = [0.6, 0.05, -0.01, 0.9]
 
 export const DEFAULT_DURATION = 0.6
